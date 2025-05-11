15239d34a204d9ddc40b88c3ad75380a1e3caf62
pato-rosso <patriciosimonrosso@gmail.com>
2025-05-11 19:33:26 -0300

    initialize project

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+//import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("Main");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
